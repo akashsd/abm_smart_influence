e3804a4c117e11487ad968662b561033b1296711
Akash Sood <akashsd@umich.edu>
2021-04-26 10:54:18 +0530

    123

--- social_influence_model_akash.py
+++ social_influence_model_akash.py
@@ -16,13 +16,15 @@ import random
 
 
 timesteps = 100
-simulations = 100
+simulations = 5
 prevalence_array = np.zeros([timesteps, simulations])
 
 N = 10
 k = 2
 p = 0.2
 seed = 100
+
+likert = 5
 
 
 def initialize():
@@ -37,16 +39,17 @@ def initialize():
     # each node gets attribute called state where 0 := susceptible
     nx.set_node_attributes(g, 0, 'state')
     for n in g.nodes:
-        g._node[n]['state'] = random.randint(0, 1)
-    g._node[1]['state'] = 1  # node 1 starts with state 1 := influencer signal
+        g._node[n]['state'] = random.randint(0, likert - 1)
+    g._node[0]['state'] = random.randint(
+        0, likert - 1)  # node 0 := influencer signal
 
     # each node gets attribute called influencer
     nx.set_node_attributes(g, 0, 'influencer')
-    g._node[1]['influencer'] = 1  # node 1 is the influencer
+    g._node[0]['influencer'] = 1  # node 0 is the influencer
 
     labels = {}
     labels[1] = "Influencer"
-    for i in range(2, len(g.nodes)):
+    for i in range(1, len(g.nodes)):
         labels[i] = "Seeker"
 
     nextg = g.copy()
@@ -60,30 +63,28 @@ def update():
     nextg = deepcopy(g)  # current and next time steps are totally separate
 
     for a in g.nodes:
+        value = 0
+        total = 0
+
+        for b in g.neighbors(a):
+            value = value + g._node[b]['state']
+            total += 1
+        avg = value/total
+
         if g._node[a]['influencer'] == 1:  # if influencer
             nextg._node[a]['influencer'] = 1
-            nextg._node[a]['state'] = 1
+            nextg._node[a]['state'] = g._node[a]['state']
 
         if g._node[a]['influencer'] == 0:  # if seeker
             nextg._node[a]['influencer'] = 0
 
-            value = 0
-            total = 0
-            for b in g.neighbors(a):
-                if g._node[b]['state'] == 1:
-                    value += 1
-                    total += 1
-                if g._node[b]['state'] == 0:
-                    total += 1
-
-            avg = value/total
-
-            if avg < 0.5:
-                nextg._node[a]['state'] = 0
-            if avg == 0.5:
-                nextg._node[a]['state'] = randint(0, 2)
-            if avg > 0.5:
-                nextg._node[a]['state'] = 1
+            for i in range(0, likert):
+                if abs(avg-i) < 0.5:
+                    nextg._node[a]['state'] = i
+                if (avg-i) == 0.5:
+                    nextg._node[a]['state'] = random.randint(i, i+1)
+                if (avg-i) == -0.5:
+                    nextg._node[a]['state'] = random.randint(i-1, i)
 
         if g._node[a]['state'] == 1:
             curprev += 1
@@ -95,7 +96,7 @@ def update():
 def observe():
     global g, prevalence
     cla()
-    nx.draw_networkx(g, cmap=cm.Wistia, vmin=0, vmax=1,
+    nx.draw_networkx(g, cmap='Wistia', vmin=0, vmax=1,
                      #labels = labels, fontcolor = "whitesmoke", font_size = 20,
                      node_color=[g._node[i]['state'] for i in g.nodes],
                      pos=g.pos)
@@ -112,9 +113,35 @@ for i in range(0, simulations):    # loop over all simulations
     # store the resulting simulation in prevalence_array
     prevalence_array[:, i] = prevalence
 
+
+prevalence_avg=prevalence_array.mean(axis=1)
+#takes row average
+
+plt.scatter(range(len(prevalence_avg)),
+            prevalence_avg,)
+plt.xlabel("Time")
+plt.ylabel("Prevalence of Influencer Signal")
+plt.text(2, .5, "Simulation Number: %d" % i)
+plt.show()
+
+plt.savefig('plot%d.png' % i)
+
+
 # Graph Prevalence
-colors = np.random.randint(100, size=(len(prevalence)))
-prevalence_graph = scatter(range(len(prevalence)),prevalence)
-xlabel("Time")
-ylabel("Prevalence of Influencer Signal")
-show(prevalence_graph)
+# for i in range(0,simulations):
+#     plt.scatter(range(len(prevalence_array[:, i])),
+#                 prevalence_array[:, i])
+#     plt.xlabel("Time")
+#     plt.ylabel("Prevalence of Influencer Signal")
+#     plt.text(2, .5, "Simulation Number: %d" %i)
+#     # plt.show()
+#     plt.savefig('plot%d.png' %i)
+
+
+
+# colors = np.random.randint(100, size=(len(prevalence)))
+# plt.scatter(range(len(prevalence)),
+#             prevalence, c=colors, cmap='viridis')
+# xlabel("Time")
+# ylabel("Prevalence of Influencer Signal")
+# plt.show()
